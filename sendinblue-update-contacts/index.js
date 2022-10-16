1f168835e82b25d4cd63a9444cd20ac9577c8249
fabien_renaud <frd.frenaud@gmail.com>
2022-10-16 02:58:25 +0200

    feat: updateContacts & deleteContacts

--- sendinblue-update-contacts/index.js
+++ sendinblue-update-contacts/index.js
@@ -1,34 +1,69 @@
 import axios from 'axios';
 
-async function getContacts() {
+const {FREE_SUBSCRIPTION_LIST_ID, PREMIUM_SUBSCRIPTION_LIST_ID, SENDINBLUE_API_KEY} = process.env;
+
+const headers = {headers: {'api-key': SENDINBLUE_API_KEY}};
+
+async function getFreeSubscriptionContacts() {
     try {
-        console.info('Getting contacts from database');
-        return Promise.resolve([
-            {
-                email: 'contact@fabienrenaud.fr',
-                attributes: {nom: 'RENAUD', prénom: 'Fabien', sms: ''},
-                listIds: [2],
-                updateEnabled: true
-            }
-        ]);
+        console.info('Getting free subscription contacts from database');
+        const contacts = await Promise.resolve([{email: 'free@fabienrenaud.fr', attributes: {NOM: 'RENAUD', PRENOM: 'Fabien', SMS: ''}}]);
+        return contacts.map((contact) => ({...contact, listIds: [FREE_SUBSCRIPTION_LIST_ID], updateEnabled: true}));
     } catch (error) {
-        throw new Error('An error occurred while getting contacts from database', error);
+        throw new Error('An error occurred while getting free subscription contacts from database', error);
+    }
+}
+
+async function getPremiumSubscriptionContacts() {
+    try {
+        console.info('Getting premium subscription contacts from database');
+        const contacts = await Promise.resolve([
+            {email: 'premium@fabienrenaud.fr', attributes: {NOM: 'RENAUD', PRENOM: 'Fabien', SMS: ''}},
+            {email: 'terminated@fabienrenaud.fr', attributes: {NOM: 'RENAUD', PRENOM: 'Fabien', SMS: ''}}
+        ]);
+        return contacts.map((contact) => ({...contact, listIds: [PREMIUM_SUBSCRIPTION_LIST_ID], updateEnabled: true}));
+    } catch (error) {
+        throw new Error('An error occurred while getting premium subscription contacts from database', error);
+    }
+}
+
+async function getTerminatedSubscriptionContacts() {
+    try {
+        console.info('Getting terminated subscription contacts from database');
+        return Promise.resolve([{email: 'terminated@fabienrenaud.fr', attributes: {NOM: 'RENAUD', PRENOM: 'Fabien', SMS: ''}}]);
+    } catch (error) {
+        throw new Error('An error occurred while getting terminated subscription contacts from database', error);
     }
 }
 
 async function updateContacts(contacts) {
     try {
-        console.info('Updating contacts in sendinblue');
-        await axios.post('https://api.sendinblue.com/v3/contacts', contacts);
+        console.info(`Updating ${contacts.length} contacts in sendinblue`);
+        await Promise.all(contacts.map(async (contact) => await axios.post('https://api.sendinblue.com/v3/contacts', contact, headers)));
     } catch (error) {
+        console.error(error.response);
         throw new Error('An error occurred while updating contacts in sendinblue', error);
+    }
+}
+
+async function deleteContacts(contacts) {
+    try {
+        console.info(`Deleting ${contacts.length} contacts in sendinblue`);
+        await Promise.all(contacts.map(async (contact) => await axios.delete(`https://api.sendinblue.com/v3/contacts/${contact.email}`, headers)));
+    } catch (error) {
+        console.error(error.response);
+        throw new Error('An error occurred while deleting contacts in sendinblue', error);
     }
 }
 
 async function sendinblueUpdateContacts(request, response) {
     try {
-        const contacts = await getContacts();
-        // await updateContacts(contacts);
+        const freeSubscriptionContacts = await getFreeSubscriptionContacts();
+        const premiumSubscriptionContacts = await getPremiumSubscriptionContacts();
+        const terminatedSubscriptionContacts = await getTerminatedSubscriptionContacts();
+
+        await updateContacts([...freeSubscriptionContacts, ...premiumSubscriptionContacts]);
+        await deleteContacts(terminatedSubscriptionContacts);
         response.send(201);
     } catch (error) {
         console.error(error);
